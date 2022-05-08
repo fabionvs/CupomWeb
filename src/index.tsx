6875f153c7f5828e9cf96289a8aa76837da397b1
fabio.garcia <fabio.garcia@dpu.def.br>
2022-05-08 03:49:36 -0300

    vai

--- src/index.tsx
+++ src/index.tsx
@@ -54,6 +54,7 @@ function PrivateRoute({ ...rest }) {
                         </>
                     ) : (
                         <>
+                            <Route path="*" element={<Navigate to="/login" />} />
                             <Route path="/login" element={<Login />} />
                             <Route path="/register" element={<Register />} />
                         </>
